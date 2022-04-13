861c370551208d6f00f950645e3bbf67d0187c45
Azevedo-Gustavo <gustavoazevedo499@gmail.com>
2022-04-12 21:49:10 -0300

    1ª versão

---
+++ MODULO  6 - JAVASCRIPT/7. Operadores Comparativos/script.js
@@ -0,0 +1,8 @@
+// == igual a
+// === valor igual e igual (número e texto)
+// != não é igual
+// !== não igual ou não igual
+// > maior que
+// < menor que
+// >= maior ou igual a
+// <= menor ou igual a
